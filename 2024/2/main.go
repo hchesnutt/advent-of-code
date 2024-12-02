e56e964034c3829020b6efa2f34c414ac56197ec
hchesnutt <t.henry.chesnutt@gmail.com>
2024-12-02 09:44:23 -0800

    day 2

---
+++ 2024/2/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"strings"
+)
+
+func main() {
+
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+	reports := strings.Split(input, "\n")
+
+}
